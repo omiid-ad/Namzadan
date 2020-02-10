b7616714c6636bb08b27935d980837e41d9199a6
Omid Adib <akbaradibfar4@gmail.com>
2020-02-10 21:19:43 +0330

    customize admin panel to be more user friendly

--- mainapp/admin.py
+++ mainapp/admin.py
@@ -1,8 +1,49 @@
 from django.contrib import admin
+from django.contrib.auth.models import Group, User
+
 from .models import *
 
-admin.site.register(Candidate)
+
+class CandidateA(admin.ModelAdmin):
+    list_display = ('full_name', 'code', 'party', 'zone')
+    list_filter = ('zone', 'party',)
+    search_fields = ['code', 'full_name']
+    fieldsets = (
+        (None, {
+            'fields': (('full_name', 'father_name', 'code', 'party', 'zone'), 'profile_picture_url')
+        }),
+        ('اطلاعات بیشتر', {
+            'classes': ('collapse',),
+            'fields': ('nickname', 'motto')
+        }),
+    )
+
+
+# class ResumeA(admin.ModelAdmin):
+#     list_display = ('full_name', 'code', 'party', 'zone')
+
+
+class ProvinceA(admin.ModelAdmin):
+    list_display = ('name',)
+
+
+class CityA(admin.ModelAdmin):
+    list_display = ('name', 'province')
+    list_filter = ('province',)
+
+
+class ZoneA(admin.ModelAdmin):
+    list_display = ('name', 'city',)
+    list_filter = ('city',)
+
+
+admin.site.unregister(Group)
+admin.site.unregister(User)
+
+admin.site.register(Candidate, CandidateA)
 admin.site.register(Resume)
-admin.site.register(Province)
-admin.site.register(City)
-admin.site.register(Zone)
+admin.site.register(Province, ProvinceA)
+admin.site.register(City, CityA)
+admin.site.register(Zone, ZoneA)
+
+admin.site.site_header = "نامزدان دات آی آر"
