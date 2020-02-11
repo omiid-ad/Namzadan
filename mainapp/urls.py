40d842d69c3a00d663e2da05aef9bcd202f40d32
Omid Adib <akbaradibfar4@gmail.com>
2020-02-11 11:58:15 +0330

    add search function + filter by zone and/or city + pagination

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -2,10 +2,9 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('landing/', home, name="home"),
     path('', home, name="home"),
     path('contact-us/', contact_us, name="contact_us"),
-    path('candidates/', candidates, name="candidates"),
+    path('candidates/', home, name="candidates"),
     # detail for each candidate url
 
 
--- mainapp/views.py
+++ mainapp/views.py
@@ -1,16 +1,56 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.db.models import Q
+from django.core.paginator import Paginator
+
+from .models import Candidate, City, Zone
 
 
 def home(request):
-    return HttpResponse("landing page")
+    all_candidates = Candidate.objects.all()
+    all_cities = City.objects.all().order_by('name')
+    all_zones = Zone.objects.all().order_by('city__name')
+    paginator = Paginator(all_candidates, 12)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+    if request.method == 'GET':
+        return render(request, 'mainapp/all-candidates.html',
+                      {'all_candidates': all_candidates, 'all_cities': all_cities, 'all_zones': all_zones,
+                       'page_obj': page_obj})
+    else:
+        if 'search' in request.POST:
+            search_text = str(request.POST['search'])
+            all_candidates = Candidate.objects.filter(
+                Q(full_name__contains=search_text) | Q(nickname__contains=search_text) |
+                Q(father_name__contains=search_text) | Q(code__contains=search_text) |
+                Q(party__contains=search_text) |
+                Q(zone__city__name__contains=search_text))
+            return render(request, 'mainapp/all-candidates.html',
+                          {'all_candidates': all_candidates, 'all_cities': all_cities, 'all_zones': all_zones})
+        city_pk = request.POST['city']
+        zone_pk = request.POST['zone']
+        if city_pk == "none":
+            if zone_pk == "none":
+                return render(request, 'mainapp/all-candidates.html',
+                              {'all_candidates': all_candidates, 'all_cities': all_cities, 'all_zones': all_zones})
+        if city_pk == "none":
+            if zone_pk != "none":
+                all_candidates = Candidate.objects.filter(zone_id=zone_pk)
+                return render(request, 'mainapp/all-candidates.html',
+                              {'all_candidates': all_candidates, 'all_cities': all_cities, 'all_zones': all_zones})
+        if city_pk != "none":
+            if zone_pk == "none":
+                all_candidates = Candidate.objects.filter(zone__city_id=city_pk)
+                return render(request, 'mainapp/all-candidates.html',
+                              {'all_candidates': all_candidates, 'all_cities': all_cities, 'all_zones': all_zones})
+        if city_pk != "none":
+            if zone_pk != "none":
+                all_candidates = Candidate.objects.filter(zone__city_id=city_pk, zone_id=zone_pk)
+                return render(request, 'mainapp/all-candidates.html',
+                              {'all_candidates': all_candidates, 'all_cities': all_cities, 'all_zones': all_zones})
 
 
 def contact_us(request):
     return HttpResponse("contact us page")
-
-
-def candidates(request):
-    return HttpResponse("candidates page")
 
 # search views
