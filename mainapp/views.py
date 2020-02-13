b4a7ced3c763d99a20c047347bfa5fc43a9a5f9d
Omid Adib <akbaradibfar4@gmail.com>
2020-02-13 11:56:37 +0330

    change search by city to search by province

--- mainapp/views.py
+++ mainapp/views.py
@@ -5,12 +5,12 @@ from django.db.models import Q
 from django.core.paginator import Paginator
 from Namzadan import settings
 
-from .models import Candidate, City, Zone, Resume
+from .models import Candidate, City, Zone, Resume, Province
 
 
 def home(request):
     all_candidates = Candidate.objects.all()
-    all_cities = City.objects.all().order_by('name')
+    all_provinces = Province.objects.all().order_by('name')
     all_zones = Zone.objects.all().order_by('city__name')
     MEDIA_URL = settings.MEDIA_URL
     paginator = Paginator(all_candidates, 12)
@@ -18,7 +18,7 @@ def home(request):
     page_obj = paginator.get_page(page_number)
     if request.method == 'GET':
         return render(request, 'mainapp/all-candidates.html',
-                      {'all_candidates': all_candidates, 'all_cities': all_cities, 'all_zones': all_zones,
+                      {'all_candidates': all_candidates, 'all_provinces': all_provinces, 'all_zones': all_zones,
                        'page_obj': page_obj, 'MEDIA_URL': MEDIA_URL})
     else:
         if 'search' in request.POST:
@@ -32,45 +32,45 @@ def home(request):
             page_number = request.GET.get('page')
             page_obj = paginator.get_page(page_number)
             return render(request, 'mainapp/all-candidates.html',
-                          {'all_candidates': all_candidates, 'all_cities': all_cities, 'all_zones': all_zones,
+                          {'all_candidates': all_candidates, 'all_provinces': all_provinces, 'all_zones': all_zones,
                            'MEDIA_URL': MEDIA_URL,
                            'page_obj': page_obj})
-        city_pk = request.POST['city']
+        province_pk = request.POST['province']
         zone_pk = request.POST['zone']
-        if city_pk == "none":
+        if province_pk == "none":
             if zone_pk == "none":
                 return render(request, 'mainapp/all-candidates.html',
-                              {'all_candidates': all_candidates, 'all_cities': all_cities, 'all_zones': all_zones,
+                              {'all_candidates': all_candidates, 'all_provinces': all_provinces, 'all_zones': all_zones,
                                'MEDIA_URL': MEDIA_URL,
                                'page_obj': page_obj})
-        if city_pk == "none":
+        if province_pk == "none":
             if zone_pk != "none":
                 all_candidates = Candidate.objects.filter(zone_id=zone_pk)
                 paginator = Paginator(all_candidates, 12)
                 page_number = request.GET.get('page')
                 page_obj = paginator.get_page(page_number)
                 return render(request, 'mainapp/all-candidates.html',
-                              {'all_candidates': all_candidates, 'all_cities': all_cities, 'all_zones': all_zones,
+                              {'all_candidates': all_candidates, 'all_provinces': all_provinces, 'all_zones': all_zones,
                                'MEDIA_URL': MEDIA_URL,
                                'page_obj': page_obj})
-        if city_pk != "none":
+        if province_pk != "none":
             if zone_pk == "none":
-                all_candidates = Candidate.objects.filter(zone__city_id=city_pk)
+                all_candidates = Candidate.objects.filter(zone__city__province_id=province_pk)
                 paginator = Paginator(all_candidates, 12)
                 page_number = request.GET.get('page')
                 page_obj = paginator.get_page(page_number)
                 return render(request, 'mainapp/all-candidates.html',
-                              {'all_candidates': all_candidates, 'all_cities': all_cities, 'all_zones': all_zones,
+                              {'all_candidates': all_candidates, 'all_provinces': all_provinces, 'all_zones': all_zones,
                                'MEDIA_URL': MEDIA_URL,
                                'page_obj': page_obj})
-        if city_pk != "none":
+        if province_pk != "none":
             if zone_pk != "none":
-                all_candidates = Candidate.objects.filter(zone__city_id=city_pk, zone_id=zone_pk)
+                all_candidates = Candidate.objects.filter(zone__city__province_id=province_pk, zone_id=zone_pk)
                 paginator = Paginator(all_candidates, 12)
                 page_number = request.GET.get('page')
                 page_obj = paginator.get_page(page_number)
                 return render(request, 'mainapp/all-candidates.html',
-                              {'all_candidates': all_candidates, 'all_cities': all_cities, 'all_zones': all_zones,
+                              {'all_candidates': all_candidates, 'all_provinces': all_provinces, 'all_zones': all_zones,
                                'MEDIA_URL': MEDIA_URL,
                                'page_obj': page_obj})
 
