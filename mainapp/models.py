55c1c36b3436b0e37a141d46743ae8b9419351a8
Omid Adib <akbaradibfar4@gmail.com>
2020-02-15 18:50:43 +0330

    remove requierd from father name and code + remove unique from code

--- mainapp/models.py
+++ mainapp/models.py
@@ -10,7 +10,7 @@ class Province(models.Model):
 
 class City(models.Model):
     province = models.ForeignKey(Province, on_delete=models.CASCADE, verbose_name="استان مربوطه")
-    name = models.CharField(max_length=50,unique=True, verbose_name="نام شهر")
+    name = models.CharField(max_length=50, unique=True, verbose_name="نام شهر")
 
     def __str__(self):
         return self.name + " - " + self.province.name
@@ -41,15 +41,15 @@ class Candidate(models.Model):
                                             verbose_name="عکس پروفایل")  # 120*162px
     full_name = models.CharField(max_length=50, verbose_name="نام و نام خانوادگی")
     nickname = models.CharField(max_length=50, blank=True, verbose_name="نام مستعار(مشهور به)")
-    father_name = models.CharField(max_length=50, verbose_name="نام پدر")
-    code = models.CharField(max_length=10, unique=True, verbose_name="کد انتخاباتی")
+    father_name = models.CharField(max_length=50, verbose_name="نام پدر", blank=True, null=True)
+    code = models.CharField(max_length=10, verbose_name="کد انتخاباتی", blank=True, null=True)
     motto = models.TextField(blank=True, verbose_name="شعار انتخاباتی")
     party = models.CharField(max_length=25, choices=PARTIES, default=UNKNOWN, verbose_name="حزب")
     # zone = models.ForeignKey(Zone, on_delete=models.CASCADE, verbose_name="ناحیه", default=None)
     city = models.ForeignKey(City, on_delete=models.CASCADE, verbose_name="ناحیه انتخاباتی", default=None)
 
     def __str__(self):
-        return self.full_name + " - " + self.code
+        return self.full_name
 
     def save(self, *args, **kwargs):
         if not self.profile_picture_url or self.profile_picture_url is None:
@@ -83,8 +83,8 @@ class Resume(models.Model):
     degree = models.CharField(max_length=50, blank=True, choices=DEGREES, default=UNKNOWN, verbose_name="مدرک تحصیلی")
     field = models.CharField(max_length=50, blank=True, verbose_name="رشته تحصیلی")
     moarefi_barnameha = models.TextField(blank=True, verbose_name="معرفی برنامه ها")
-    #birth_locate = models.CharField(max_length=50, blank=True, verbose_name="محل تولد")
-    #biography = models.TextField(blank=True, verbose_name="زندگی نامه")
+    # birth_locate = models.CharField(max_length=50, blank=True, verbose_name="محل تولد")
+    # biography = models.TextField(blank=True, verbose_name="زندگی نامه")
     records = models.TextField(blank=True, verbose_name="سوابق")
     banner1 = models.ImageField(upload_to="banners", default="banners/default.jpg", blank=True,
                                 verbose_name="پوستر۱")  # 240*320
